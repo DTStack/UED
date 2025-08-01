80bce9a12dfd114e7b85d529ce0d1ddee32c8366
liuyi <liuyi@dtstack.com>
2025-08-01 17:25:07 +0800

    feat: console.log app version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.scss';
 import 'antd/dist/antd.css';
+import {version} from '../../package.json';
 
 export default function MyApp({ Component, pageProps }) {
+    console.log(
+        `%cApp current version: v${version}`,
+        'font-family: Cabin, Helvetica, Arial, sans-serif;text-align: left;font-size:32px;color:#B21212;'
+    );
+
     return <Component {...pageProps} />
 }
